162ca818b36702cf4d14b8c1017fa5ec55e757fe
Sarvesh Shejwadkar <sarvesh@sjinnovation.com>
2015-12-28 20:39:29 +0530

    some changes

--- create_product.php
+++ create_product.php
@@ -5,18 +5,46 @@
 	require __DIR__.'/vendor/autoload.php';
 	use phpish\shopify;
 
-	require __DIR__.'/includes/conf.php';
+	require __DIR__.'/conf.php';
 
 	require __DIR__.'/spreadsheet-reader/SpreadsheetReader.php';
 
 	require __DIR__.'/includes/functions.php';
 
-	$shopify = shopify\client(SHOPIFY_SHOP, SHOPIFY_APP_API_KEY, SHOPIFY_APP_PASSWORD, true);
+	//$shopify = shopify\client(SHOPIFY_SHOP, SHOPIFY_APP_API_KEY, SHOPIFY_APP_PASSWORD, true);
 
-	$ProductsReader = new SpreadsheetReader('products.xlsx');
+	$ProductsReader = new SpreadsheetReader('Book1.xlsx');
 
-	$products = getProductsArr($ProductsReader);
+	//$products = getProductsArr($ProductsReader);
 
+   	$dbhost = 'localhost';
+   	$dbuser = 'root';
+   	$dbpass = 'root';
+   	$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   	if(! $conn )
+   	{
+    	die('Could not connect: ' . mysql_error());
+   	}
+   	echo 'Connected successfully';
+   	mysql_select_db( 'shopify', $conn );
+
+   	foreach ($ProductsReader as $key => $value) {
+	   	$sql = "INSERT INTO log (data) VALUES ('" . mysql_real_escape_string(json_encode($value)) . "')";
+		$retval = mysql_query( $sql, $conn );
+		if(! $retval )
+		{
+		  die('Could not enter data: ' . mysql_error());
+		}
+		echo "Entered data successfully\n";
+	}
+
+   	mysql_close($conn);
+
+	// foreach ($ProductsReader as $key => $value) {
+	// 	$products[] = $value;
+	// }
+
+	exit;
 	foreach($products as $product) {
 
 	    $data = array('product' => $product);
